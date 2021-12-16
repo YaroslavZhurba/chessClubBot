fe2d1e0f1e1334700e0cce1b07b5aec75827bcb7
yaroslav <zhurba_ys@mail.ru>
2021-12-16 23:48:08 +0300

    delete unused code

--- Util/main.py
+++ Util/main.py
@@ -1,5 +1,3 @@
-import telepot
-import json
 import sys
 import os
 
@@ -13,19 +11,3 @@ print(sys.path)
 import handler
 
 handler.run()
-# while True:
-#     updates = TelegramBot.getUpdates()
-#     if len(updates) > 0:
-#         update_id = updates[0]['update_id']
-#         break
-# flag = True
-# while flag:
-#     updates = TelegramBot.getUpdates(update_id)
-#     if len(updates) > 0:
-#         for update in updates:
-#             if handler.make_decision(update):
-#                 flag = False
-#                 update_id += len(updates)
-#                 TelegramBot.getUpdates(update_id)
-#                 break
-#         update_id += len(updates)
